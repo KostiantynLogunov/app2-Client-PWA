ba2caeeff896749bffae1341c591eee7140ef3bd
KostiantynLogunov <777koc@gmail.com>
2018-12-17 00:02:20 +0200

    made fun with clients

--- src/components/clients/List.vue
+++ src/components/clients/List.vue
@@ -17,10 +17,10 @@
                         <div class="modal-body">
                             <form @submit.prevent="addNewClient()">
                                 <div class="form-group text-left">
-                                    <label for="recipient-name" class="col-form-label">Name:</label>
+                                    <label for="recipient_name" class="col-form-label">Name:</label>
                                     <input type="text"
                                            class="form-control"
-                                           id="recipient-name"
+                                           id="recipient_name"
                                            v-model="new_client.first_name"
                                            name="Name"
                                            v-validate="'required|min: 3|max: 26|alpha_num'"
@@ -28,10 +28,10 @@
                                     >
                                 </div>
                                 <div class="form-group text-left">
-                                    <label for="recipient-email" class="col-form-label">Email:</label>
-                                    <input type="text"
+                                    <label for="recipient_email" class="col-form-label">Email:</label>
+                                    <input type="email"
                                            class="form-control"
-                                           id="recipient-email"
+                                           id="recipient_email"
                                            v-model="new_client.email"
                                            required
                                            placeholder="Enter your email address"
@@ -98,7 +98,65 @@
                         </td>
                         <td>
                             <!--<router-link :to="`/clients/${client.id}`">View</router-link>-->
-                            <button class="btn btn-warning"><i class="far fa-edit"></i></button>
+                            <!--<button class="btn btn-warning"><i class="far fa-edit"></i></button>-->
+                            <button type="button" class="btn btn-warning" data-toggle="modal" :data-target="'#exampleModalCenter2' + client.id" @click="editClient(client)">
+                                <i class="far fa-edit"></i>
+                            </button>
+                            <!-- Modal -->
+                            <div class="modal fade" :id="'exampleModalCenter2' + client.id" tabindex="-1" role="dialog" :aria-labelledby="'exampleModalCenterTitle2' + client.id" aria-hidden="true">
+                                <div class="modal-dialog modal-dialog-centered" role="document">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" :id="'exampleModalCenterTitle2' + client.id">Editing Client</h5>
+                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                <span aria-hidden="true">&times;</span>
+                                            </button>
+                                        </div>
+                                        <div class="modal-body">
+                                            <form @submit.prevent="UpdateClient()">
+                                                <div class="form-group text-left">
+                                                    <label :for="'client_name' + client.id"
+                                                           class="col-form-label"
+                                                    >
+                                                        Name:
+                                                    </label>
+                                                    <input type="text"
+                                                           class="form-control"
+                                                           :id="'client_name' + client.id"
+                                                           v-model="edit_client.first_name"
+                                                           name="Name"
+                                                    >
+                                                </div>
+                                                <div class="form-group text-left">
+                                                    <label :for="'client_email' + client.id"
+                                                           class="col-form-label"
+                                                    >
+                                                        Email:
+                                                    </label>
+                                                    <input type="email"
+                                                           class="form-control"
+                                                           :id="'client_email' + client.id"
+                                                           v-model="edit_client.email"
+                                                           required
+                                                           name="email"
+                                                           required
+                                                    >
+                                                </div>
+                                                <ul class="text-left text-danger">
+                                                    <li v-for="error in errors.all()">{{ error }}</li>
+                                                </ul>
+                                                <div class="modal-footer">
+                                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                                    <button type="submit" class="btn btn-primary" :disabled="errors.items.length > 0">Update</button>
+                                                </div>
+                                            </form>
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </div>
+
+
                             <button class="btn btn-danger" @click="DeleteClient(client.id)"><i class="far fa-trash-alt"></i></button>
                         </td>
                     </tr>
@@ -120,6 +178,11 @@
                     first_name: null,
                     email: null
                 },
+                edit_client: {
+                    id: null,
+                    first_name: null,
+                    email: null
+                },
                 client_contacts: false,
             }
         },
@@ -134,6 +197,32 @@
 
         methods:
         {
+            UpdateClient()
+            {
+                axios.put(config.apiUrl + '/clients/' + this.edit_client.id, this.edit_client, {
+                    headers: {
+                        "Authorization": `Bearer ${ this.$store.getters.currentUser.token }`
+                    }
+                })
+                    .then((response) => {
+                        this.$store.dispatch('getClients');
+                        $('#exampleModalCenter2' + this.edit_client.id).modal('hide');
+                        this.edit_client.id = null;
+                        this.edit_client.first_name = null;
+                        this.edit_client.email = null;
+                    })
+                    .catch((err) => {
+                        reject("Something Wrong ...");
+                    })
+            },
+
+            editClient(client)
+            {
+                this.edit_client.id = client.id;
+                this.edit_client.first_name = client.first_name;
+                this.edit_client.email = client.email;
+            },
+
             showContacts(clent_id){
                 if (this.client_contacts == clent_id)
                     this.client_contacts = false;
